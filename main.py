c495d9eca4e39c9598b8e37aea935adcbd45d864
Janfiffik <janfiffik@gmail.com>
2024-02-23 19:15:54 +0100

    Creating third function that creates gross income , budget etc... for movies by webscraping imdb

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import csv
+import random
 import pandas as pd
 import requests
 import json
 from bs4 import BeautifulSoup
 from datetime import datetime
+import time as tm
 
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
 def imdb_top_movies():
@@ -29,8 +32,9 @@ def imdb_top_movies():
 
 def movie_json_data(movies_names, api_key="YOUR KEY"):
     """For getting raw JSON data from IMDb database. You need pass as argument list with movie names.
-       its creates JSON file
-       You can get your API key by signing in to IMDb database.
+       its creates JSON file.
+
+       !!!!!_You can get your API key by signing in to IMDb database_!!!!!
     """
     movies_data = []
     for movie in movies_names:
@@ -46,6 +50,48 @@ def movie_json_data(movies_names, api_key="YOUR KEY"):
 
     with open("data/movies_raw_data.json", "w") as json_file:
         json.dump(movies_data, json_file, indent=4, separators=(',', ': '))
+
+
+def movie_prices(movies):
+    """It returns 2D list of lists with: budgets_list, gross_us,
+       gross_world, opening_us_canada
+       Argument accept: list with imdb ID of movies
+    """
+    budgets_list = []
+    gross_us = []
+    gross_world = []
+    opening_us_canada = []
+
+    for movie in movies:
+        url_movie_price = f"https://www.imdb.com/title/{movie}/?ref_=chttp_t_1"
+        headers = {'User-Agent': 'Chrome/58.0.3029.110', 'Accept-Language': 'en-US,en;q=0.9'}
+        response = requests.get(url_movie_price, headers=headers)
+        response_data = BeautifulSoup(response.content, 'html.parser')
+        movie_budget = response_data.find_all(class_="ipc-metadata-list-item__list-content-item")
+        movie_price = [movie.text for movie in movie_budget]
+
+        movie_price = [price.replace(',', "") for price in movie_price if '$' in price]
+        movie_price = [price.replace('(estimated)', '') for price in movie_price]
+        movie_price = [price.replace('$', '') for price in movie_price]
+
+        try:
+            budgets_list.append(movie_price[0])
+        except IndexError:
+            budgets_list.append("0")
+        try:
+            gross_us.append(movie_price[1])
+        except IndexError:
+            gross_us.append("0")
+        try:
+            opening_us_canada.append(movie_price[2])
+        except IndexError:
+            opening_us_canada.append("0")
+        try:
+            gross_world.append(movie_price[3])
+        except IndexError:
+            gross_world.append("0")
+        tm.sleep(random.choice(range(0, 1)))
+    return [budgets_list, gross_us, gross_world, opening_us_canada]
 
 # Converting JSON to csv file----------------------------------------------------------------------------------------
 with open('data/movies_raw_data.json', 'r', encoding='utf-8') as file:
@@ -81,7 +127,6 @@ movies_language = [movie["Language"] for movie in data]
 movies_country = [movie["Country"] for movie in data]
 
 movies_awards = [movie["Awards"] for movie in data]
-print(movies_awards)
 
 movies_imdb_rating = [movie["imdbRating"] for movie in data]
 movies_imdb_rating = [float(rating) for rating in movies_imdb_rating]                   # Done
@@ -89,31 +134,38 @@ movies_imdb_rating = [float(rating) for rating in movies_imdb_rating]           
 movies_imdb_votes = [movie["imdbVotes"] for movie in data]
 movies_imdb_votes = [int(vote.replace(',', "")) for vote in movies_imdb_votes]          # Done
 
-# Convert str to int and get rid of 'N/A' value.
-movies_cinema_earnings = [movie["BoxOffice"] for movie in data]
-movies_cinema_earnings = [earning.replace(',', "") for earning in movies_cinema_earnings]  # Get rid of ','
-new_earnings = []                                                                          # New temporary list
-for earning in movies_cinema_earnings:
-    if earning != 'N/A':
-        new_earnings.append(int(earning.replace('$', "")))
-    else:
-        new_earnings.append(0)
-movies_cinema_earnings = new_earnings
+movies_imdb_id = [movie["imdbID"] for movie in data]
+#
+# # Creating data Frame-----------------------------------------------
+# data_csv = pd.DataFrame()
+# data_csv.index = movies_titles
+# data_csv["Released"] = movies_release
+# data_csv["Genre"] = movies_genre
+# data_csv["Length"] = movies_length
+# data_csv["Rating"] = movies_rated
+# data_csv["Country"] = movies_country
+# data_csv["Language"] = movies_language
+# data_csv["Director"] = movies_director
+# data_csv["Writers"] = movies_writers
+# data_csv["Actors"] = movies_actors
+# data_csv["Rewards"] = movies_awards
+# data_csv["Imdb_Rating"] = movies_imdb_rating
+# data_csv["Imdb_Votes"] = movies_imdb_votes
+# data_csv["imdb_ID"] = movies_imdb_id
 
-data_csv = pd.DataFrame()
-data_csv.index = movies_titles
-data_csv["Released"] = movies_release
-data_csv["Genre"] = movies_genre
-data_csv["Length"] = movies_length
-data_csv["Rating"] = movies_rated
-data_csv["Country"] = movies_country
-data_csv["Language"] = movies_language
-data_csv["Director"] = movies_director
-data_csv["Writers"] = movies_writers
-data_csv["Actors"] = movies_actors
-data_csv["Rewards"] = movies_awards
-data_csv["Imdb_Rating"] = movies_imdb_rating
-data_csv["Imdb_Votes"] = movies_imdb_votes
-data_csv["Cinema_earnings"] = movies_cinema_earnings
+# budget_and_price = movie_prices(movies_imdb_id)
+# data_csv["Movie_Budget"] = budget_and_price[0]
+# data_csv["Gross_in_US"] = budget_and_price[1]
+# data_csv["World_gross_income"] = budget_and_price[2]
+# data_csv["Opening_US_CANADA"] = budget_and_price[3]
+# data_csv.to_csv("data/movies_data.csv", index=True)
 
-print(data_csv.head(5))
+csv_data = pd.read_csv('data/movies_data.csv')
+print(csv_data.shape)
+
+print(csv_data.columns)
+
+print(csv_data[['Unnamed: 0', "Movie_Budget", 'Gross_in_US', 'World_gross_income', 'Opening_US_CANADA']])
+
+
+
