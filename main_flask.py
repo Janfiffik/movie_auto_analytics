26917f6667a4ef56973e1e7566bee78c66bf198f
Janfiffik <janfiffik@gmail.com>
2024-02-28 09:34:08 +0100

    Adding Flask app for showing data analysis

--- main_flask.py
+++ main_flask.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import pandas as pd
+
+
+def read_data():
+    with open("data/movies_data.csv") as file:
+        data = pd.read_csv(file)
+        return data
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    data = read_data()
+    movie_names = data["Titles"]
+    return render_template("index.html", titles=movie_names)
 
 if __name__ == "__main__":
     app.run(debug=True)
