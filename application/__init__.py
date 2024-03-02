d09450534c5fd0d4c383206630855e4e38928db8
Janfiffik <janfiffik@gmail.com>
2024-03-02 08:33:51 +0100

    Creating dashboard web app for data analysis

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expensesDB.db'
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moviesDB.db'
 app.config['SECRET_KEY'] = "fdgdfgn;dfbdngfmfgksmnbsrtihnmmsnbksbsnfgjnhbd"
 
 db = SQLAlchemy(app)
--- application/models.py
+++ application/models.py
@@ -1,24 +1,29 @@
 from application import db
+from datetime import datetime
 
 
 class MovieDatabase(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     Title: str = db.Column(db.String, nullable=False)
-    Released = db.Column(db.Datetime)
-    Genre = db.Column(db.String)
-    Length = db.Column(db.Integer)
-    Age_Rating = db.Column(db.String)
-    Country = db.Column(db.String)
-    Writers = db.Column(db.String)
-    Actors = db.Column(db.String)
-    Imdb_Rating = db.Column(db.Integer)
-    Imdb_Votes = db.Column(db.Integer)
-    Movie_Budget = db.Column(db.Integer)
-    Gross_in_Us = db.Column(db.Integer)
-    World_Gross = db.Column(db.Integer)
-    Opening_US_CAN = db.Column(db.Integer)
-    Oscar_Wins = db.Column(db.Integer)
-    Oscar_Nomination = db.Column(db.Integer)
-    Other_Wins = db.Column(db.Integer)
-    Other_Nomination = db.Column(db.Integer)
+    Released = db.Column(db.DateTime, nullable=False, default="Unknown")
+    Genre = db.Column(db.String, nullable=False, default="Genre")
+    Length = db.Column(db.Integer, nullable=False, default=0)
+    Age_Rating = db.Column(db.String, nullable=False, default="Age_Rating")
+    Country = db.Column(db.String, nullable=False, default="Country")
+    Language = db.Column(db.String, nullable=False, default="Unknown")
+    Director = db.Column(db.String, nullable=False, default="Director")
+    Writers = db.Column(db.String, nullable=False, default="Writers")
+    Actors = db.Column(db.String, nullable=False, default="actor_name")
+    Imdb_Rating = db.Column(db.Integer, nullable=False, default=0)
+    Imdb_Votes = db.Column(db.Integer, nullable=False, default=0)
+    Movie_Budget = db.Column(db.Integer, nullable=False, default=0)
+    Gross_in_Us = db.Column(db.Integer, nullable=False, default=0)
+    World_Gross = db.Column(db.Integer, nullable=False, default=0)
+    Opening_US_CAN = db.Column(db.Integer, nullable=False, default=0)
+    Oscar_Wins = db.Column(db.Integer, nullable=False, default=0)
+    Oscar_Nomination = db.Column(db.Integer, nullable=False, default=0)
+    Other_Wins = db.Column(db.Integer, nullable=False, default=0)
+    Nomination_Total = db.Column(db.Integer, nullable=False, default=0)
 
+    def __str__(self):
+        return self.id
